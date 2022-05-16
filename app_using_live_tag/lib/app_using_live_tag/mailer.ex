8762dca216824b401395ad1c7ffe52589751292e
mhamza427 <muhammad.hamza@invozone.com>
2022-05-16 12:04:03 +0500

    12-05-2022

---
+++ app_using_live_tag/lib/app_using_live_tag/mailer.ex
@@ -0,0 +1,3 @@
+defmodule AppUsingLiveTag.Mailer do
+  use Swoosh.Mailer, otp_app: :app_using_live_tag
+end
---
+++ app_using_live_tag/lib/app_using_live_tag_web/live/live_view_prac.ex
@@ -0,0 +1,36 @@
+defmodule AppUsingLiveTagWeb.LiveViewPrac do
+  use Phoenix.LiveView
+  # def render(assigns) do
+  #   ~H"""
+  #   <section class="row">
+  #     <h4>
+  #       Counter <%= @count%>
+  #     </h4>
+  #     <button
+  #       style="width: 25;"
+  #       phx-click="for_increment"
+  #     >
+  #       +
+  #     </button>
+  #   </section>
+  #   """
+  # end
+  def mount(_params, _session, socket) do
+    {:ok,
+    socket
+    |> assign(count: 0)
+  }
+  end
+  def handle_event("for_increment", _params, socket) do
+    {:noreply, socket
+    |> update(:count, &(&1+1))}
+  end
+  def handle_event("for_decrement", _params, socket) do
+    {:noreply, socket
+    |> update(:count, &(&1-1))}
+  end
+  def handle_event("for_clear", _params, socket) do
+    {:noreply, socket
+    |> assign(count: 0)}
+  end
+end
